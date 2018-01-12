be8e32b1c066f65e3c82ad569e93b3c307227eab
shane ryan <onyx.shane@gmail.com>
2018-01-12 15:27:52 -0800

    add second working api to display desired doctors contact information

--- js/api.js
+++ js/api.js
@@ -7,20 +7,45 @@ export function getDoctor(callback, input) {
   console.log(symptomInput);
   let request = new XMLHttpRequest();
   let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptomInput}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`;
-
   request.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
       let response = JSON.parse(this.responseText);
       let results = response.data;
       console.log(results);
-      console.log(results[0].profile.bio);
       results.forEach(function(i){
         doctorsArray.push(i.profile.last_name);
         console.log(doctorsArray);
-      })
+      });
       callback(doctorsArray);
     }
-  }
+  };
+  request.open("get", url, true);
+  request.send();
+}
+
+export function getInfo(hollerback, doctorInput) {
+  const infoArray = [];
+  const dr = doctorInput;
+  console.log(doctorInput);
+  let request = new XMLHttpRequest();
+  let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${dr}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`;
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      let response = JSON.parse(this.responseText);
+      console.log(response);
+      let results = response.data;
+      infoArray.push(results[0].profile.first_name);
+      infoArray.push(results[0].profile.last_name);
+      infoArray.push(results[0].profile.bio);
+      infoArray.push(results[0].practices[0].accepts_new_patients);
+      infoArray.push(results[0].practices[0].visit_address.street);
+      infoArray.push(results[0].practices[0].visit_address.city);
+      infoArray.push(results[0].practices[0].visit_address.state);
+      infoArray.push(results[0].practices[0].visit_address.zip);
+      infoArray.push(results[0].practices[0].phones[0].number);
+      hollerback(infoArray);
+    }
+  };
   request.open("get", url, true);
   request.send();
 }
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,4 +1,4 @@
-import { getDoctor } from './../js/api.js';
+import { getDoctor, getInfo } from './../js/api.js';
 
 $(document).ready(function(){
   $('#symptom').click(function(event){
@@ -8,5 +8,27 @@ $(document).ready(function(){
   });
   function callback(doctorsArray){
     console.log(doctorsArray);
+    doctorsArray.forEach(function(doc){
+      $('#doc-for-symptom').append('<li> Dr. ' + doc + '</li>');
+
+    });
+  }
+  $('#doctor').click(function(event){
+    const doctorInput = $('#doctor-query').val();
+    console.log(doctorInput);
+    getInfo(hollerback, doctorInput);
+  });
+  function hollerback(infoArray){
+    console.log(infoArray);
+    infoArray.forEach(function(i){
+      if (i === true) {
+        $('#doc-info').append('<li>' + 'accepting new patients' + '</li>');
+      }else if( i === false) {
+        $('#doc-info').append('<li> not accepting new patients </li>');
+      }else {
+          $('#doc-info').append('<li>' + i + '</li>');
+      }
+
+    });
   }
 });
